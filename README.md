d9bda9284409c2dc8d907923b1c637ad467d2aac
Filipe "Ender" Araujo <death.enderman@gmail.com>
2020-04-28 18:14:26 -0300

    begining my express server

---
+++ README.md
@@ -0,0 +1,5 @@
+# express
+# server
+First trial to use express
+this project objective is to draw something in one 
+computer and see and other
---
+++ public/sketch.js
@@ -0,0 +1,6 @@
+let express = require('express')//inserting the module
+let app = express()//calling the class
+let server = app.listen(3000)// starting the server
+app.use(express.static ('public'))//sending the dat to the client
+cosole.log('server running and data sended')
+//---------------------------------------------------
